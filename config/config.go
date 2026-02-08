53e0b3b57cb288e5186f125a76fe00de5a865dd0
AngelRattner <angelrattner@gmail.com>
2026-02-09 00:40:26 +0200

    fix validations

--- config/config.go
+++ config/config.go
@@ -9,6 +9,7 @@ type Config struct {
 	SMTPHost           string
 	SMTPPort           string
 	SMTPSenderEmail    string
+	SMTPSenderName     string // New: Configurable Sender Name
 	SMTPSenderUser     string // For AUTH
 	SMTPSenderPass     string // For AUTH
 	InsecureSkipVerify bool
@@ -20,6 +21,7 @@ func Load() *Config {
 		SMTPHost:           getEnv("SMTP_HOST", "localhost"),
 		SMTPPort:           getEnv("SMTP_PORT", "1025"),
 		SMTPSenderEmail:    getEnv("SMTP_SENDER_EMAIL", "president@whitehouse.gov"),
+		SMTPSenderName:     getEnv("SMTP_SENDER_NAME", "Donald Trump"),
 		SMTPSenderUser:     getEnv("SMTP_USER", ""),
 		SMTPSenderPass:     getEnv("SMTP_PASS", ""),
 		InsecureSkipVerify: getEnv("INSECURE_SKIP_VERIFY", "true") == "true",
